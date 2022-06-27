e20ef4a5d13872b552ec5e6a6b6b61e81e4279d1
Kim Black <kimblackaz@gmail.com>
2022-06-27 12:33:26 -0600

    Don't click anything but the first card
    Co-authored-by: Avie <avieeich@users.noreply.github.com>

---
+++ src/js/cards.js
@@ -0,0 +1,96 @@
+import {
+  renderListWithTemplate
+} from "./utils.js";
+
+// function revealCard(file) {
+//   picture = querySelector(".overlay");
+//   picture.classList.toggle("hide");
+// }
+
+export default class Cards {
+  constructor(listElement) {
+    this.listElement = listElement;
+  }
+  async init() {
+    // Grab a random list of cases from the API here...
+    // const list =  require('./data.json');
+    // console.log(list);
+    // if (list.length === 0) {
+    //   this.listElement.innerHTML =
+    //     "<li> Your cart is empty</li>";
+    // } else {
+    //   this.renderList(list);
+    // }
+
+    var list = [
+      {
+        "id": 1,
+        "type": "Missing Person",
+        "victim": "John Doe",
+        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "contact": "website.com",
+        "picture": "./img/placeholder.png"
+      },
+       {
+        "id": 2,
+        "type": "Missing Person",
+        "victim": "John Doe",
+        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "contact": "website.com",
+        "picture": "./img/placeholder.png"
+      },
+      {
+        "id": 3,
+        "type": "Missing Person",
+        "victim": "John Doe",
+        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "contact": "website.com",
+        "picture": "./img/placeholder.png"
+      },
+      {
+        "id": 4,
+        "type": "Missing Person",
+        "victim": "John Doe",
+        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "contact": "website.com",
+        "picture": "./img/placeholder.png"
+      },
+      {
+        "id": 5,
+        "type": "Missing Person",
+        "victim": "John Doe",
+        "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "contact": "website.com",
+        "picture": "./img/placeholder.png"
+      }
+    ]
+    
+
+    this.renderList(list);
+  }
+  prepareTemplate(template, file) {
+    template.querySelector("#victim-name").innerHTML = file.victim;
+    template.querySelector("#case-type").innerHTML = file.type;
+    template.querySelector("#profile").src = file.picture;
+    template.querySelector(".summary").innerHTML = file.summary;
+    template.querySelector("#website").innerHTML = file.contact;
+    template.querySelector(".card_information").setAttribute("data-id", file.id);
+    template
+      .querySelector(".card")
+      .addEventListener("click", () => {
+        var picture = document.querySelector(".overlay");
+        picture.classList.toggle("hide");
+      });
+    return template;
+  }
+  renderList(list) {
+    this.listElement.innerHTML = "";
+    const template = document.getElementById("card-template");
+    renderListWithTemplate(
+      template,
+      this.listElement,
+      list,
+      this.prepareTemplate
+    );
+  }
+}
