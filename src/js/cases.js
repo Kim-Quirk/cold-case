d90e360e672391f251066b4cdb2462622559936a
Avie <avie.eich@gmail.com>
2022-07-15 12:51:55 -0600

    is actually game

--- src/js/cases.js
+++ src/js/cases.js
@@ -1,25 +1,28 @@
 import ExternalServices from "./externalServices.js";
-import { alertMessage } from "./utils.js";
+import { alertMessage, getLocalStorage } from "./utils.js";
 
 export default class Cases {
-  constructor() {
-    this.services = new ExternalServices();
-    this.caseData = "";
-  }
-  async init() {
-    this.caseData = this.services.fetchJSON("cases");
-    console.log(this.caseData);
-  }
-  async filter(type, date, status) {
-    // const caseByType = null;
-    // const caseByDate = null;
-    // const caseByStatus = null;
-    // var filter = { caseType: type, caseDate: date, caseStatus: status };
-    // filteredCases = this.caseData.filter(function (item) {
-    //   for (var key in filter) {
-    //     if (item[key] === undefined || item[key] != filter[key]) return false;
-    //   }
-    //   return true;
-    // });
-  }
+    constructor() {
+      this.token = null;
+      this.services = new ExternalServices();
+      this.cases;
+    }
+    async init() {
+      var results = await this.services.fetchJSON("cases");
+      this.cases = results;
+      console.log("cases", this.cases);
+      return this.cases;
+    }
+  // async filter(type, date, status) {
+  //   // const caseByType = null;
+  //   // const caseByDate = null;
+  //   // const caseByStatus = null;
+  //   // var filter = { caseType: type, caseDate: date, caseStatus: status };
+  //   // filteredCases = this.caseData.filter(function (item) {
+  //   //   for (var key in filter) {
+  //   //     if (item[key] === undefined || item[key] != filter[key]) return false;
+  //   //   }
+  //   //   return true;
+  //   // });
+  // }
 }
